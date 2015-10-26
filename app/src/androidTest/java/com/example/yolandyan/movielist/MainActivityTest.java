ba0be4794e680cd6f210c795bce4ee52454e4b71
Yoland <yoland68@gmail.com>
2015-10-25 18:07:08 -0700

    Pass both ids and poster paths to image adapter

--- app/src/androidTest/java/com/example/yolandyan/movielist/MainActivityTest.java
+++ app/src/androidTest/java/com/example/yolandyan/movielist/MainActivityTest.java
@@ -1,5 +1,6 @@
 package com.example.yolandyan.movielist;
 
+import android.net.Uri;
 import android.test.ActivityInstrumentationTestCase2;
 import android.widget.GridView;
 
@@ -8,8 +9,13 @@ import android.widget.GridView;
  */
 public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
 
+    private final String IMAGE_BASE_URL = "image.tmdb.org";
+    private final String SORT_PARAM = "sort_by";
+    private final String SORT = "popularity.desc";
+
     private MainActivity mMainActivity;
     private GridView mGridView;
+
 
     public MainActivityTest() {
         super(MainActivity.class);
@@ -35,6 +41,19 @@ public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActiv
     public void testGridViewAdapter() {
         ImageAdapter adapter = (ImageAdapter) mGridView.getAdapter();
         assertNotNull(adapter);
-        assertEquals(adapter.getCount(), adapter.getImageLinks().length);
+        assertEquals(adapter.getCount(), adapter.getMovieData().size());
+        assertEquals(adapter.getCount(), adapter.getMovieIds().length);
     }
+
+    public void testGridViewItem() {
+        ImageAdapter adapter = (ImageAdapter) mGridView.getAdapter();
+        for (int i = 0; i < adapter.getCount(); i++) {
+            String posterUrlString = (String) mGridView.getItemAtPosition(i);
+            Uri posterUri = Uri.parse(posterUrlString);
+            assertEquals(posterUri.getAuthority(), "image.tmdb.org");
+        }
+    }
+
+    //How to do unit testing? e.g whether json data is being processed correctly?
+    //how to test image views
 }
