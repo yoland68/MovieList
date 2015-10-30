83a3ecfde27846b38e1048aae423dfa2e1f6a442
Yoland <yoland68@gmail.com>
2015-10-29 23:11:34 -0700

    Actionbar and rotation support

--- app/src/main/java/com/example/yolandyan/movielist/DetailActivity.java
+++ app/src/main/java/com/example/yolandyan/movielist/DetailActivity.java
@@ -1,7 +1,9 @@
 package com.example.yolandyan.movielist;
 
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 
 /**
@@ -13,9 +15,13 @@ public class DetailActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.activity_detail_toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar ab = getSupportActionBar();
+        ab.setDisplayHomeAsUpEnabled(true);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.detail_fragement, new DetailActivityFragment())
+                    .add(R.id.container_detail, new DetailActivityFragment())
                     .commit();
         }
     }
--- app/src/main/java/com/example/yolandyan/movielist/MainActivity.java
+++ app/src/main/java/com/example/yolandyan/movielist/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -14,9 +15,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.activity_main_toolbar);
+        setSupportActionBar(toolbar);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.main_fragment, new MainActivityFragment())
+                    .add(R.id.container, new MainActivityFragment())
                     .commit();
         }
     }
