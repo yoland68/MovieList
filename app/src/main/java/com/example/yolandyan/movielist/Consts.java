5a24a768e043ec59a2771eaff05d0f3a73c256a2
Yoland <yoland68@gmail.com>
2015-10-29 23:23:26 -0700

    Readded Consts.java

---
+++ app/src/main/java/com/example/yolandyan/movielist/Consts.java
@@ -0,0 +1,19 @@
+package com.example.yolandyan.movielist;
+
+/**
+ * Created by yolandyan on 10/29/15.
+ */
+public class Consts {
+    public static final String BASE_URL = "http://api.themoviedb.org/3/";
+    public static final String IMAGE_BASE_URL = "http://image.tmdb.org/t/p";
+    public static final String MOVIE_PATH = "movie";
+    public static final String POPULAR_PATH = "popular";
+    public static final String RATING_PATH = "top_rated";
+    public static final String API_KEY_PARAM = "api_key";
+    public static final String API_KEY = "[PUT YOUR OWN API KEY]";
+    public static final String IMAGE_SIZE = "w500";
+
+    public static final String SORT_BY_POPULARITY = "popularity";
+    public static final String SORT_BY_RATING = "rating";
+    public static final String SORT_KEY = "sort";
+}
