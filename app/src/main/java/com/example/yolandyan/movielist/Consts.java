681709ad9e0ff1223f72e57faa4e99e4cf949b48
Yoland <yoland68@gmail.com>
2015-10-30 21:34:30 -0700

    change consts

--- app/src/main/java/com/example/yolandyan/movielist/Consts.java
+++ app/src/main/java/com/example/yolandyan/movielist/Consts.java
@@ -10,7 +10,7 @@ public class Consts {
     public static final String POPULAR_PATH = "popular";
     public static final String RATING_PATH = "top_rated";
     public static final String API_KEY_PARAM = "api_key";
-    public static final String API_KEY = "964a973c564ea29df85b4cb40c6bec10";
+    public static final String API_KEY = "[PUT YOUR OWN API KEY]";
     public static final String IMAGE_SIZE = "w500";
 
     public static final String SORT_BY_POPULARITY = "popularity";
